fa70d4fe576dcdfff1c967da91e970fa8682974b
theguspassos <theguspassos@gmail.com>
2024-12-21 12:22:37 -0300

    update type registry

---
+++ TradingCards/Converters/TypeRegistry.cs
@@ -0,0 +1,18 @@
+﻿namespace TradingCards.Converters;
+
+public class TypeRegistry
+{
+    readonly Dictionary<string, Type> mappings = [];
+    public void Register<T>(string type) => mappings.Add(type, typeof(T));
+    public Type GetType(string type) => mappings[type];
+}
+
+public class CardTypeRegistry : TypeRegistry
+{
+}
+
+public class FilterTypeRegistry : TypeRegistry
+{
+}
+
+
